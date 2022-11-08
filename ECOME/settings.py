76628ff7cf0d763b90290a6875299bf2f4b54688
Great Special <sirgreatspecial@gmail.com>
2022-11-08 23:29:43 +0100

    deploying to railway

--- ECOME/settings.py
+++ ECOME/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i)v+**%!2@7yh1&&pn57ptv17ix7+bt&d1cqrkwyx_vm%-hlwa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Setting up for payment support using stripe The Publishable key is used on the frontend while the Secret key is used in the backend
 STRIPE_SECRET_KEY = 'sk_test_51LzQwaCJA0lCvXM0KTpA7v4kax7O0QXZ9NtD7PaKdFRuXpxGuXuJpG6ZsGLdZYXhuMqKIAdabge8zlBiuUvYUHGY00syL32Zna'
